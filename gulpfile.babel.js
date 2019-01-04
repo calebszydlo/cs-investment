2066b53ae4a1678e8df7b663262931d321189507
Caleb Szydlo <connect@calebszydlo.com>
2019-01-04 07:53:28 -0600

    adding bourbon and neat

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,23 @@
 import { src, dest } from 'gulp';
-import yargs from 'yargs';
-import sass from 'gulp-sass';
-import cleanCss from 'gulp-clean-css';
-import gulpif from 'gulp-if';
-const PRODUCTION = yargs.argv.prod;
+
+var autoprefix = require('gulp-autoprefixer'),
+    gulp       = require('gulp'),
+    yargs      = require('yargs'),
+    bourbon    = require('bourbon').includePaths,
+    neat       = require('bourbon-neat').includePaths,
+    sass       = require('gulp-sass'),
+    cleanCss   = require('gulp-clean-css'),
+    gulpif     = require('gulp-if');
+
+const PRODUCTION = yargs.argy.prod;
 
 export const styles = () => {
   return src('src/stylesheets/style.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      sourcemaps: true,
+      includePaths: [bourbon, neat]
+    }).on('error', sass.logError))
+    .pipe(autoprefix('last 2 versions'))
     .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
     .pipe(dest('dist/css'));
 }
